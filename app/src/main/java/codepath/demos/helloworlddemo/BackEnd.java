85ea61c4d9e42198a8447b0be1ee22e2e6e8a3fa
aaron <aaronys@uci.edu>
2019-05-09 19:50:58 -0700

    jaeven pls save my sanity

--- app/src/main/java/codepath/demos/helloworlddemo/BackEnd.java
+++ app/src/main/java/codepath/demos/helloworlddemo/BackEnd.java
@@ -2,10 +2,24 @@ package codepath.demos.helloworlddemo;
 
 import android.graphics.Rect;
 
+import java.util.concurrent.TimeUnit;
+
 public class BackEnd {
     public void fall(Rect getRect, int TetrisSpace[][]){
-        int i = 0;
-        i++;
+        int i = 0, j = 0;
+        int width = TetrisSpace[1].length;
+        TetrisSpace[0][width] = 1;
+        while(TetrisSpace[i+1][width] == 0){
+            TetrisSpace[i][width] = 0;
+            i++;
+            TetrisSpace[i][width] = 1;
+            try {
+                TimeUnit.SECONDS.sleep(1);
+            }catch(InterruptedException e){
+                e.printStackTrace();
+            }
+        }
+
 
     }
 }
