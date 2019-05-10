3e4e3a21db89f9edb3e18807aaeb37d7acdf1b77
jlaron <jlaron@uci.edu>
2019-05-10 03:39:29 -0700

    Added Shapes and Move methods in Custom View

--- app/src/main/java/codepath/demos/helloworlddemo/MainThread.java
+++ app/src/main/java/codepath/demos/helloworlddemo/MainThread.java
@@ -23,14 +23,8 @@ public class MainThread extends Thread{
 
     @Override
     public void run(){
-        long startTime;
-        long timeMillis = 1000/MAX_FPS;
-        long waitTime;
-        int frameCount = 0;
-        long totalTime = 0;
-        long targetTime = 1000/MAX_FPS;
+
         while(running){
-            startTime = System.nanoTime();
             canvas = null;
             try {
                 canvas = this.surfaceHolder.lockCanvas();
@@ -48,21 +42,8 @@ public class MainThread extends Thread{
                     catch(Exception e){e.printStackTrace();}
                 }
 
-            }
-            timeMillis = (System.nanoTime() - startTime)/1000000;
-            waitTime = targetTime - timeMillis;
-            try{
-                if(waitTime>0)
-                    this.sleep(waitTime);
-            }catch(Exception e) {e.printStackTrace();}
 
-            totalTime += System.nanoTime() - startTime;
-            frameCount++;
-            if(frameCount == MAX_FPS){
-                averageFPS = 1000/((totalTime/frameCount)/1000000);
-                frameCount = 0;
-                totalTime =0;
-                System.out.println(averageFPS);
+
 
             }
         }
